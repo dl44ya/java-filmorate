51a0d747c232ca7da6f599ce4aafd5a78b21e468
Oxana <oxanakj909@gmail.com>
2026-06-29 13:38:51 +0300

    initial commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,103 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final static String EMAIL_ERROR = "Почта не может быть пустой и должно содержать @";
+    private final static String LOGIN_ERROR = "Логин не может быть пустым и содержать пробелы";
+    private final static String BIRTHDAY_ERROR = "Дата рождения не может быть в будущем";
+    private final static String ID_ERROR = "Id должен быть указан";
+    private final static String FIELDS_ERROR = "Ошибка в заполнении полей";
+    private final static String USER_NOT_FOUND_ERROR = "Пользователь не найден";
+    private final static String DUPLICATED_USER_ERROR = "Пользователь с такой почтой уже зарегестрирован";
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        log.info("Создание нового пользователя");
+        if (user.getEmail() == null || !user.getEmail().contains("@") || user.getEmail().isBlank()) {
+            log.warn("Неверный формат почты");
+            throw new ValidationException(EMAIL_ERROR);
+        }
+        if (!users.values().stream()
+                .filter(value -> value.getEmail().equals(user.getEmail()))
+                .toList().isEmpty()) {
+            log.warn("Пользователь уже существует");
+            throw new ValidationException(DUPLICATED_USER_ERROR);
+        }
+        if (user.getLogin() == null || user.getLogin().contains(" ") || user.getLogin().isBlank()) {
+            log.warn("Неверный формат логина");
+            throw new ValidationException(LOGIN_ERROR);
+        }
+        LocalDate now = LocalDate.now();
+        if (user.getBirthday().isAfter(now)) {
+            log.warn("Неверная дата рождения");
+            throw new ValidationException(BIRTHDAY_ERROR);
+        }
+        user.setId(getNextId());
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        log.info("Новый пользователь создан");
+        return user;
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        log.info("Обновлене данных пользоваеля");
+        if (newUser.getId() == null) {
+            log.warn("ID не передан");
+            throw new ValidationException(ID_ERROR);
+        }
+        LocalDate now = LocalDate.now();
+        if (users.containsKey(newUser.getId())) {
+            if ((newUser.getEmail() == null || !newUser.getEmail().contains("@")) ||
+                    (newUser.getLogin() == null || newUser.getLogin().contains(" ")) ||
+                    (newUser.getBirthday().isAfter(now))) {
+                log.warn("Ошибка заполнения полей");
+                throw new ValidationException(FIELDS_ERROR);
+            }
+            User oldUser = users.get(newUser.getId());
+            oldUser.setEmail(newUser.getEmail());
+            oldUser.setLogin(newUser.getLogin());
+            oldUser.setBirthday(newUser.getBirthday());
+            if (newUser.getName() == null || newUser.getName().isBlank()) {
+                oldUser.setName(newUser.getLogin());
+            } else {
+                oldUser.setName(newUser.getName());
+            }
+            log.info("Данные пользователя обновлены");
+            return oldUser;
+        }
+        log.warn("Пользователь не найден");
+        throw new ValidationException(USER_NOT_FOUND_ERROR);
+    }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        log.info("Получение данных о всех пользователях");
+        return users.values();
+    }
+}
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,311 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.AssertionsKt.assertNotNull;
 
 @SpringBootTest
 class FilmorateApplicationTests {
+    private FilmController filmController;
+    private Film validFilm;
+    private UserController userController;
+    private User validUser;
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeEach
+    void setData() {
+        filmController = new FilmController();
+        validFilm = new Film();
+        validFilm.setName("Film");
+        validFilm.setDescription("Film description");
+        validFilm.setReleaseDate(LocalDate.of(1895, Month.DECEMBER, 28));
+        validFilm.setDuration(100L);
 
+        userController = new UserController();
+        validUser = new User();
+        validUser.setEmail("user@email.ru");
+        validUser.setLogin("user333");
+        validUser.setName("User User");
+        validUser.setBirthday(LocalDate.of(1980, Month.AUGUST, 1));
+    }
+
+    /// Film
+    @Test
+    void createFilm_ShouldSucceed_AllFieldsWalid() {
+        Film newFilm = filmController.create(validFilm);
+
+        assertDoesNotThrow(() -> filmController.create(validFilm));
+        assertNotNull(newFilm.getId());
+        assertEquals(validFilm.getName(), newFilm.getName());
+        assertEquals(validFilm.getDescription(), newFilm.getDescription());
+        assertEquals(validFilm.getReleaseDate(), newFilm.getReleaseDate());
+        assertEquals(validFilm.getDuration(), newFilm.getDuration());
+    }
+
+    @Test
+    void createFilm_ShouldNotSucceed_BlankName() {
+        validFilm.setName("");
+        Film invalidFilm = validFilm;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> filmController.create(invalidFilm));
+        assertEquals("Название не может быть пустым", ex.getMessage());
+    }
+
+    @Test
+    void createFilm_ShouldNotSucceed_NullName() {
+        validFilm.setName(null);
+        Film invalidFilm = validFilm;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> filmController.create(invalidFilm));
+        assertEquals("Название не может быть пустым", ex.getMessage());
+    }
+
+    @Test
+    void createFilm_ShouldNotSucceed_LongDescription() {
+        validFilm.setDescription("a".repeat(201));
+        Film invalidFilm = validFilm;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> filmController.create(invalidFilm));
+        assertEquals("Mаксимальная длина описания — 200 символов", ex.getMessage());
+    }
+
+    @Test
+    void createFilm_ShouldNotSucceed_InvalidReleaseDate() {
+        validFilm.setReleaseDate(LocalDate.of(1895, Month.DECEMBER, 27));
+        Film invalidFilm = validFilm;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> filmController.create(invalidFilm));
+        assertEquals("Дата релза не может быть раньше 28 декабря 1895 года", ex.getMessage());
+    }
+
+    @Test
+    void createFilm_ShouldNotSucceed_InvalidDuration() {
+        validFilm.setDuration(0L);
+        Film invalidFilm = validFilm;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> filmController.create(invalidFilm));
+        assertEquals("Продолжительность фильма должна быть положительным числом", ex.getMessage());
+    }
+
+    @Test
+    void updateFilm_ShouldSucceed_AllFieldsWalid() {
+        Film oldFilm = filmController.create(validFilm);
+        Film newFilm = new Film();
+        newFilm.setId(oldFilm.getId());
+        newFilm.setName("Film 2");
+        newFilm.setDescription("Film description 2");
+        newFilm.setReleaseDate(LocalDate.of(1999, Month.DECEMBER, 28));
+        newFilm.setDuration(120L);
+
+        Film result = filmController.update(newFilm);
+
+        assertEquals(oldFilm.getId(), result.getId());
+        assertEquals(result.getName(), newFilm.getName());
+        assertEquals(result.getDescription(), newFilm.getDescription());
+        assertEquals(result.getReleaseDate(), newFilm.getReleaseDate());
+        assertEquals(result.getDuration(), newFilm.getDuration());
+    }
+
+    @Test
+    void updateFilm_ShouldNotSucceed_InvalidID() {
+        Film newFilm = new Film();
+        newFilm.setName("Film 2");
+        newFilm.setDescription("Film description 2");
+        newFilm.setReleaseDate(LocalDate.of(1999, Month.DECEMBER, 28));
+        newFilm.setDuration(120L);
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> filmController.update(newFilm));
+
+        assertEquals("Id должен быть указан", ex.getMessage());
+    }
+
+    @Test
+    void updateFilm_ShouldNotSucceed_InvalidField() {
+        Film oldFilm = filmController.create(validFilm);
+        Film newFilm = new Film();
+        newFilm.setId(oldFilm.getId());
+        newFilm.setName("Film 2");
+        newFilm.setDescription("Film description 2");
+        newFilm.setReleaseDate(LocalDate.of(1999, Month.DECEMBER, 28));
+        newFilm.setDuration(-200L);
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> filmController.update(newFilm));
+
+        assertEquals("Ошибка в заполнении полей", ex.getMessage());
+    }
+
+    /// User
+    @Test
+    void createUser_ShouldSucceed_AllFieldsWalid() {
+        assertDoesNotThrow(() -> userController.create(validUser));
+
+        userController = new UserController();
+        User newUser = userController.create(validUser);
+
+        assertNotNull(newUser.getId());
+        assertEquals(newUser.getEmail(), validUser.getEmail());
+        assertEquals(newUser.getName(), validUser.getName());
+        assertEquals(newUser.getLogin(), validUser.getLogin());
+        assertEquals(newUser.getBirthday(), validUser.getBirthday());
+    }
+
+    @Test
+    void createUser_ShouldNotSucceed_DuplicatedUser() {
+        userController.create(validUser);
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.create(validUser));
+        assertEquals("Пользователь с такой почтой уже зарегестрирован", ex.getMessage());
+    }
+
+    @Test
+    void createUser_ShouldNotSucceed_BlankEmail() {
+        validUser.setEmail("");
+        User invalidUser = validUser;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.create(invalidUser));
+        assertEquals("Почта не может быть пустой и должно содержать @", ex.getMessage());
+    }
+
+    @Test
+    void createUser_ShouldNotSucceed_NullEmail() {
+        validUser.setEmail(null);
+        User invalidUser = validUser;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.create(invalidUser));
+        assertEquals("Почта не может быть пустой и должно содержать @", ex.getMessage());
+    }
+
+    @Test
+    void createUser_ShouldNotSucceed_InvalidEmail() {
+        validUser.setEmail("example.email.ru");
+        User invalidUser = validUser;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.create(invalidUser));
+        assertEquals("Почта не может быть пустой и должно содержать @", ex.getMessage());
+    }
+
+    @Test
+    void createUser_ShouldNotSucceed_BlankLogin() {
+        validUser.setLogin("");
+        User invalidUser = validUser;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.create(invalidUser));
+        assertEquals("Логин не может быть пустым и содержать пробелы", ex.getMessage());
+    }
+
+    @Test
+    void createUser_ShouldNotSucceed_NullLogin() {
+        validUser.setLogin(null);
+        User invalidUser = validUser;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.create(invalidUser));
+        assertEquals("Логин не может быть пустым и содержать пробелы", ex.getMessage());
+    }
+
+    @Test
+    void createUser_ShouldNotSucceed_InvalidLogin() {
+        validUser.setLogin("nhdb ki");
+        User invalidUser = validUser;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.create(invalidUser));
+        assertEquals("Логин не может быть пустым и содержать пробелы", ex.getMessage());
+    }
+
+    @Test
+    void createUser_ShouldSucceed_BlankName() {
+        validUser.setName("");
+        User newUser = userController.create(validUser);
+
+        assertEquals(newUser.getName(), newUser.getLogin());
+    }
+
+    @Test
+    void createUser_ShouldSucceed_NullName() {
+        validUser.setName(null);
+        User newUser = userController.create(validUser);
+
+        assertEquals(newUser.getName(), newUser.getLogin());
+    }
+
+    @Test
+    void createUser_ShouldNotSucceed_InvalidBirthday() {
+        LocalDate now = LocalDate.now();
+        validUser.setBirthday(now.plusDays(1));
+        User invalidUser = validUser;
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.create(invalidUser));
+        assertEquals("Дата рождения не может быть в будущем", ex.getMessage());
+    }
+
+    @Test
+    void updateUser_ShouldSucceed_AllFieldsWalid() {
+        User oldUser = userController.create(validUser);
+        User newUser = new User();
+        newUser.setId(oldUser.getId());
+        newUser.setEmail("example2@email.ru");
+        newUser.setLogin("user222");
+        newUser.setName("User User2");
+        newUser.setBirthday(LocalDate.of(2020, Month.JULY, 1));
+
+        User result = userController.update(newUser);
+
+        assertEquals(oldUser.getId(), result.getId());
+        assertEquals(oldUser.getEmail(), result.getEmail());
+        assertEquals(oldUser.getLogin(), result.getLogin());
+        assertEquals(oldUser.getName(), result.getName());
+        assertEquals(oldUser.getBirthday(), result.getBirthday());
+    }
+
+    @Test
+    void updateUser_ShouldNotSucceed_InvalidID() {
+        User newUser = new User();
+        newUser.setEmail("example2@email.ru");
+        newUser.setLogin("user222");
+        newUser.setName("User User2");
+        newUser.setBirthday(LocalDate.of(2020, Month.JULY, 1));
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.update(newUser));
+
+        assertEquals("Id должен быть указан", ex.getMessage());
+    }
+
+    @Test
+    void updateUser_ShouldNotSucceed_InvalidField() {
+        User oldUser = userController.create(validUser);
+        User newUser = new User();
+        newUser.setId(oldUser.getId());
+        newUser.setEmail("example2.email.ru");
+        newUser.setLogin("user222");
+        newUser.setName("User User2");
+        newUser.setBirthday(LocalDate.of(2020, Month.JULY, 1));
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.update(newUser));
+
+        assertEquals("Ошибка в заполнении полей", ex.getMessage());
+    }
+
+    @Test
+    void updateUser_ShouldNotSucceed_UserNotFound() {
+        User oldUser = userController.create(validUser);
+        User newUser = new User();
+        newUser.setId(oldUser.getId() + 1);
+        newUser.setEmail("example2@email.ru");
+        newUser.setLogin("user222");
+        newUser.setName("User User2");
+        newUser.setBirthday(LocalDate.of(2020, Month.JULY, 1));
+
+        ValidationException ex = assertThrows(ValidationException.class, () -> userController.update(newUser));
+
+        assertEquals("Пользователь не найден", ex.getMessage());
+    }
 }
